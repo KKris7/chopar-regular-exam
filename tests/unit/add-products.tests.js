c44f8843c432ebae1b38e558b47e1bb9ffe04f04
Kris <kris.krastanov@abv.bg>
2024-04-13 14:28:10 +0300

    fix unit tests

--- tests/unit/add-products.tests.js
+++ tests/unit/add-products.tests.js
@@ -9,7 +9,7 @@ suite('Add Products page', function() {
   });
 
   test('Product HTML form', async function() {
-    let res = await fetch("http://localhost:8080/Add-Product");
+    let res = await fetch("http://localhost:8070/Add-Product");
     let body = await res.text();
     
     let nameFieldFound = body.includes('<input id="name" type="text" name="name"/>');
@@ -24,7 +24,7 @@ suite('Add Products page', function() {
 
   test('Add valid product', async function() {
     let res = await fetch(
-      "http://localhost:8888/Add-Product",
+      "http://localhost:8070/Add-Product",
       {
         method: 'POST',
         headers: {
